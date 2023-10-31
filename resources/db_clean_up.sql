57a8e771ca33983b74096bfb2c6997de82b5d65b
“advasunk” <55065786+advasunk@users.noreply.github.com>
2023-10-30 20:29:28 -0700

    sqls to delete the data

---
+++ resources/db_clean_up.sql
@@ -0,0 +1,10 @@
+
+delete from student_incentive_plan.user_schedule_assigned_user_reln;
+
+delete from student_incentive_plan.user_schedule;
+
+delete from student_incentive_plan.user_reln;
+
+delete from student_incentive_plan.task;
+
+delete from student_incentive_plan.user_course_reln;
